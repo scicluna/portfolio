7e24661ec96b05989213544bf64ac90a51446bff
scicluna <88948700+scicluna@users.noreply.github.com>
2023-04-20 12:56:07 -0400

    done for now, some padding to the contacts list

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,11 +3,11 @@ export default function Contacts() {
         <section className="flex flex-col items-center justify-center h-full" >
             <form className="flex flex-col w-96 max-w-full" action="https://formsubmit.co/sciclunajl@gmail.com" method='POST' >
                 <label htmlFor="name" className="text-indigo-900">Name</label>
-                <input name="name" type="text" id="name" required placeholder="Name..." className="shadow-sm shadow-indigo-950 righteous" />
+                <input name="name" type="text" id="name" required placeholder="Name..." className="p-1 shadow-sm shadow-indigo-950 righteous" />
                 <label htmlFor="email" className="text-indigo-900">Email</label>
-                <input name="email" type="email" id="email" required placeholder="email@email.com..." className="shadow-sm shadow-indigo-950 righteous" />
+                <input name="email" type="email" id="email" required placeholder="email@email.com..." className="p-1 shadow-sm shadow-indigo-950 righteous" />
                 <label htmlFor="message" className="text-indigo-900">Message</label>
-                <textarea name="message" id="message" rows="5" required className="shadow-sm shadow-indigo-950 righteous" />
+                <textarea name="message" id="message" rows="5" required className="p-1 shadow-sm shadow-indigo-950 righteous" />
                 <button type="submit" className="text-indigo-900 bg-white w-1/2 self-center mt-2 rounded-md shadow-sm shadow-indigo-950 hover:bg-indigo-500 transition-all duration-200 ease-in">Send</button>
             </form>
             <div>
