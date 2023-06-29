cd892caba826f4f66a9d2ee99054f8f48582e08a
scicluna <88948700+scicluna@users.noreply.github.com>
2023-06-28 21:02:33 -0400

    updated resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,11 @@
-import resume from '../imgs/jlsprofessionalresume.pdf'
+import resume from '../imgs/jlsresume2.pdf'
 
 export default function Resume() {
     return (
         <section className="flex h-full w-full justify-center items-center">
             <object data={resume} type="application/pdf" className="h-4/5 w-4/5 shadow-sm shadow-indigo-950">
                 <p>It appears you don't have a PDF plugin for this browser. You can <a
-                    href="./src/assets/jlsresume.pdf">download
+                    href="./src/assets/jlsresume2.pdf">download
                     the PDF file</a> instead.</p>
             </object>
         </section>
